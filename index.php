803e4906497b398012169c930fba84679865e696
raulgonzalez <raulgonzalez@gmail.com>
2023-11-08 19:26:48 +0100

    Initial Commit of Padel proyect's

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <title>title</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css"/>
+    </head>
+    <body>
+        <h1>Padel</h1>
+        <p>Nombre: Raúl González Maroto y Diego Rubio Serrano</p>
+        <p>Curso: 2º DAW</p>
+        <p>Módulo: Desarrollo web en entorno servidor (DWES)</p>
+        <?php
+        ?>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    </body>
+</html>
